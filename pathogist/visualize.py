5097dcfcb2e51aef4c09fb83234c49c186174ffe
Sean La <seanla1996@gmail.com>
2018-09-26 13:44:09 -0700

    added function to visualize clusterings

--- pathogist/visualize.py
+++ pathogist/visualize.py
@@ -1,10 +1,14 @@
 import logging
-import datetime as dt
-import pandas as pd
-import numpy as np
+import pandas 
+import numpy 
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
 from scipy.cluster import hierarchy as hcl
+import sklearn
+import sklearn.manifold
+import networkx
+import random
+import itertools
 
 logger = logging.getLogger(__name__)
 
@@ -31,7 +35,7 @@ def distance_histogram(distance, name='SAMPLE', save_path=None):
 
     # Obtain distances from lower triangle
     values = distance.values
-    distances = values[np.tril_indices(len(distance.index), -1)]
+    distances = values[numpy.tril_indices(len(distance.index), -1)]
 
     plt.style.use('ggplot')
     plt.figure(0)
@@ -44,13 +48,36 @@ def distance_histogram(distance, name='SAMPLE', save_path=None):
         plt.savefig(save_path)
     plt.show()
 
-
+def visualize_clusterings(summary_clustering):
+    '''
+    Visualize the consensus clustering and correlation clusterings.
+    @param summary_clustering: A Pandas Dataframe where the index is the sample names, columns are
+                               clusterings, and entries are cluster assignments.
+    '''
+    samples = summary_clusterings.index.values
+    sample_positions = {sample: (random.randrange(0,100),random.randrange(0,100)) for sample in samples}
+    clusterings_names = summary_clusterings.columns.values
+    
+    for clustering_name in clustering_names:
+        plt.figure()
+        graph = networkx.Graph()
+        for sample in samples:
+            graph.add_node(sample,Position=sample_positions[sample]) 
+        clustering = summary_clusterings[clustering_name]
+        num_clusters = numpy.amax(clustering.values)
+        for cluster in range(1,num_clusters+1):
+            cluster_samples = clustering.index[clustering[clustering_name] == cluster].tolist()
+            for sample1,sample2 in itertools.combinations(cluster_samples,2)
+                graph.add_edge(sample1,sample2)
+        networkx.draw(graph)
+        plt.show()
+        
 def hierarchical_clustering(distance, name, metadata = None, columns = None, create_pdf = False, pdf = None):
     '''
     Runs hierarchical clustering and creates a dendrogram and MDS plot
     '''
 
-    meta = pd.read_csv(metadata, header=0, index_col=0, sep="\t")
+    meta = pandas.read_csv(metadata, header=0, index_col=0, sep="\t")
 
     # Select a subset of samples common to the distance matrix and the metadata file
     isect = [rowname for rowname in list(distance.index) if rowname in list(meta.index)]
