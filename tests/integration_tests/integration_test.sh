6c0414eea22d874649f51c571a376bc590f848c2
Sean <seanla1996@gmail.com>
2018-10-14 20:55:03 -0700

    uncommented line in integration test

--- tests/integration_tests/integration_test.sh
+++ tests/integration_tests/integration_test.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-#./pathogist.py all tests/integration_tests/test_data/pathogist-run_all-test.yaml 
+./pathogist.py all tests/integration_tests/test_data/pathogist-run_all-test.yaml 
 
 python -m unittest tests/integration_tests/test_integration.py
