114f694d7cf0cd8fd1af2175c5ae3937c3fa069e
Johnathan Kwan Long Wong <klw17@cdc2n2.jgardy.bcgsc.ca>
2019-01-29 03:35:17 -0800

    added another test and changed some assertion to be more lenient

--- tests/integration_tests/integration_test.sh
+++ tests/integration_tests/integration_test.sh
@@ -5,3 +5,7 @@ set -e
 ./PATHOGIST run tests/integration_tests/test_data/pathogist-run_all-test.yaml 
 
 python -m unittest tests/integration_tests/test_integration.py
+
+./PATHOGIST run tests/integration_tests/test2_data/pathogist-run_all-test2.yaml 
+
+python -m unittest tests/integration_tests/test2_integration.py
