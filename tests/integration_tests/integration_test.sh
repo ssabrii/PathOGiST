8c50ea4e5925bd28fe2a8785192a558ae240f4fa
jowong4 <34543031+jowong4@users.noreply.github.com>
2019-02-03 03:28:37 -0800

    Testing genotyping

--- tests/integration_tests/integration_test.sh
+++ tests/integration_tests/integration_test.sh
@@ -2,17 +2,20 @@
 
 set -e
 
-./PATHOGIST run tests/integration_tests/test_data/pathogist-run_all-test.yaml 
+conda install snippy
+snippy -h
 
-python -m unittest tests/integration_tests/test_integration.py
+#./PATHOGIST run tests/integration_tests/test_data/pathogist-run_all-test.yaml 
+
+#python -m unittest tests/integration_tests/test_integration.py
 # Run Genotyping Software
 #./PATHOGIST run tests/integration_tests/test2_data/pathogist-run_all-test2.yaml 
 
 #python -m unittest tests/integration_tests/test2_integration.py
-./PATHOGIST run tests/integration_tests/test3_data/pathogist-run_all-test3.yaml 
+#./PATHOGIST run tests/integration_tests/test3_data/pathogist-run_all-test3.yaml 
 
-python -m unittest tests/integration_tests/test3_integration.py
+#python -m unittest tests/integration_tests/test3_integration.py
 
-./PATHOGIST run tests/integration_tests/test4_data/pathogist-run_all-test4.yaml 
+#./PATHOGIST run tests/integration_tests/test4_data/pathogist-run_all-test4.yaml 
 
-python -m unittest tests/integration_tests/test4_integration.py
+#python -m unittest tests/integration_tests/test4_integration.py
