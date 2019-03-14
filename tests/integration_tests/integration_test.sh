d45442705c084242a0439999f72f8deb577ba891
jowong4 <34543031+jowong4@users.noreply.github.com>
2019-03-13 21:50:22 -0700

    Update integration_test.sh

--- tests/integration_tests/integration_test.sh
+++ tests/integration_tests/integration_test.sh
@@ -21,7 +21,7 @@ conda build conda/meta.yaml
 
 #python -m unittest tests/integration_tests/test_integration.py
 # Run Genotyping Software
-#./PATHOGIST run tests/integration_tests/test2_data/pathogist-run_all-test2.yaml 
+./PATHOGIST run tests/integration_tests/test2_data/pathogist-run_all-test2.yaml 
 
 #python -m unittest tests/integration_tests/test2_integration.py
 #./PATHOGIST run tests/integration_tests/test3_data/pathogist-run_all-test3.yaml 
