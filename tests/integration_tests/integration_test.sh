d02e8bb9f84cd5e3a43c4d4a19372d2c11454989
jowong4 <34543031+jowong4@users.noreply.github.com>
2019-03-18 19:36:31 -0700

    deactivate conda env

--- tests/integration_tests/integration_test.sh
+++ tests/integration_tests/integration_test.sh
@@ -2,27 +2,27 @@
 
 set -e
 
-wget https://repo.anaconda.com/archive/Anaconda3-2018.12-Linux-x86_64.sh -O anaconda.sh
-chmod +x anaconda.sh
-./anaconda.sh -b
-export PATH=/home/travis/anaconda3/bin:$PATH
-ls /home/travis/
-conda update --yes conda
-conda install conda-build --yes
+#wget https://repo.anaconda.com/archive/Anaconda3-2018.12-Linux-x86_64.sh -O anaconda.sh
+#chmod +x anaconda.sh
+#./anaconda.sh -b
+#export PATH=/home/travis/anaconda3/bin:$PATH
+#ls /home/travis/
+#conda update --yes conda
+#conda install conda-build --yes
 #conda install --file ../../conda/environment.yaml --yes 
-conda config --add channels defaults
-conda config --add channels bioconda
-conda config --add channels conda-forge
-conda create --name pathogist --yes 
-source activate pathogist 
-conda install pathogist --yes
+#conda config --add channels defaults
+#conda config --add channels bioconda
+#conda config --add channels conda-forge
+#conda create --name pathogist --yes 
+#source activate pathogist 
+#conda install pathogist --yes
 
 
 ./PATHOGIST run tests/integration_tests/test_data/pathogist-run_all-test.yaml 
 
 python -m unittest tests/integration_tests/test_integration.py
 # Run Genotyping Software
-./PATHOGIST run tests/integration_tests/test2_data/pathogist-run_all-test2.yaml 
+#./PATHOGIST run tests/integration_tests/test2_data/pathogist-run_all-test2.yaml 
 
 #python -m unittest tests/integration_tests/test2_integration.py
 ./PATHOGIST run tests/integration_tests/test3_data/pathogist-run_all-test3.yaml 
